a90139906ccc910b423a7412c86d0b9c5e3cd5a5
SithumiGalappaththi <sithumig99@gmail.com>
2025-06-08 21:03:42 +0530

    dfa minimization model and graphviz update

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from utils.dfa_minimization import load_dfa_minimization_model, predict_dfa_mini
 from utils.regex_to_epsilon_nfa import load_regex_to_e_nfa_model,predict_regex_to_e_nfa
 from utils.nfa_to_dfa import load_nfa_to_dfa_model,predict_nfa_to_dfa
 from utils.graphviz.graphviz_regex_to_e_nfa import epsilon_nfa_to_dot
+from utils.graphviz.graphviz_minimized_dfa import minimized_dfa_to_dot
 
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
@@ -100,21 +101,23 @@ if st.button("Convert", type="primary"):
                 
             elif selected_model['name'] == "DFA-Minimization":
                 result = predict_dfa_minimization(model,user_input)
+                graph = minimized_dfa_to_dot(result)
+                png_bytes = graph.pipe(format="png")
             elif selected_model['name'] == "NFA-to-DFA":
                 result = predict_nfa_to_dfa(model,user_input)
             
             # Display result
             st.subheader("Conversion Result:")
             st.code(result, language="text")
-            st.subheader("Generated ε-NFA")
-            # st.graphviz_chart(graph.source)
+            st.subheader("Generated Diagram:")
+            st.graphviz_chart(graph.source)
 
             if png_bytes:
                 st.subheader("Download Diagram as PNG")
                 st.download_button(
-                    label="⬇️ Download ε-NFA (PNG)",
+                    label="⬇️ Download (PNG)",
                     data=png_bytes,
-                    file_name="epsilon_nfa.png",
+                    file_name="diagram.png",
                     mime="image/png"
                 )
             
